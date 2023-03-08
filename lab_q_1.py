b5d8e8ecd43e56b872ce7048dc50f66f1391d106
Ahmet Yusuf Demir <ahmetyusufdmr88@gmail.com>
2023-03-08 22:38:34 +0300

    Create lab_q_1.py

---
+++ lab_q_1.py
@@ -0,0 +1,36 @@
+from random import randint
+
+def main(number):
+        
+    data = {}
+
+    #to get the output count
+    for i in range(number):
+
+        r1 = randint(1, 6)
+        r2 = randint(1, 6)
+
+        temp = r1+r2
+
+        if temp not in data.keys():
+            data[temp] = 1
+        else:
+            data[temp] += 1
+
+    _min = min(data.values())
+    _max = max(data.values())
+
+    #to get normalized value
+    for key, val in data.items():
+        nv = 1 + ((val - _min) * (14)) / (_max - _min)
+        data[key] = int(nv)
+
+    #to print the output
+    for key in sorted(data.keys()):
+        hey = str(key).ljust(2)
+        print(hey, data[key]*'*')
+
+for i in range(2, 5):
+    print(f"for {10**i} times: ")
+    main(10**i)
+    print()
